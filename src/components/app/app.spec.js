0403f7cdf7e67758fbb61c110393b3d078631979
Oleg <44555176+SOjuD@users.noreply.github.com>
2021-02-19 22:24:22 +0300

    fix: was changed app.spec.js

--- src/components/app/app.spec.js
+++ src/components/app/app.spec.js
@@ -3,6 +3,5 @@ import App from './app';
 
 it("should contain 'Hello World!'", () => {
     const component = shallow(<App />);
-    const wrapper = component.find('.container');
-    expect(wrapper.text).toBe('Hello World!');
+    expect(component.text()).toBe('"Hello World!"');
 });
